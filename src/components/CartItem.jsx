8bba3d5a6f80b2c5b768f0a4d6c5d4ffc2adc610
Alen <info@alenbarac.com>
2023-05-24 22:42:58 +0200

    cart components layout completed

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { ChevronDown, ChevronUp } from '../icons'
+
+const CartItem = ({ title, price, img, amount }) => {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">{price}</h4>
+        <button className="remove-btn">remove</button>
+      </div>
+      <button className="amount-btn">
+        <ChevronUp />
+      </button>
+      <p className="amount">{amount}</p>
+      <button className="amount-btn">
+        <ChevronDown />
+      </button>
+    </article>
+  )
+}
+
+export default CartItem
